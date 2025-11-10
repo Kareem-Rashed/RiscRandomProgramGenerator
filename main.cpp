7d7984e1703713f5bc3eed6b41e4ddb715de7425
JOEElmenshawy <159122991+JOEElmenshawy@users.noreply.github.com>
2025-11-10 21:51:16 +0200

    Added TestCases Used in the Report

--- main.cpp
+++ main.cpp
@@ -13,11 +13,11 @@ int main() {
     // I is for RV32I, 10 is number of instructions, 'I' is format
     //possible formats: R, I, S, B, U, J
     //will add C later
-    Generator gen('I', 10, 'S');
+    Generator gen('I', 10, 'B');
 
 
     //gen.Start();  //for single format  (change from constructor)
-    gen.GenerateAllSType();//for mixed formats
+    gen.GenerateAllBType();//for mixed formats
     gen.GenerateTCFiles();//for test case file
     gen.GenerateMem();//for mixed formats
     return 0;
